3e177b95194e3101c932124cbdf39c00854e9c13
Your Name <oz9080@gmail.com>
2018-03-14 10:08:33 +0200

    Initial Commit

---
+++ SOS App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SOS App
+//
+//  Created by Oz Arie Tal Shachar on 14/03/2018.
+//  Copyright © 2018 Oz Arie Tal Shachar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
